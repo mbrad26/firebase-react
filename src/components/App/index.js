de7faae70712bfe9551d28084423f9490f9526d6
mbrad26 <mbrad26@gmail.com>
2020-09-14 13:46:11 +0100

    add routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => {
   return (
     <Router>
-      <Navigation />
+      <div>
+        <Navigation />
+
+        <hr />
+
+        <Route exact to={ROUTES.LANDING} component={LandingPage} />
+        <Route to={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route to={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route to={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route to={ROUTES.HOME} component={HomePage} />
+        <Route to={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route to={ROUTES.ADMIN} component={AdminPage} />
+      </div>
     </Router>
+
   )
 }
 
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PasswordForgetPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PasswordForgetPage;
